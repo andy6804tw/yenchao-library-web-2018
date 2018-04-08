c1df0d3e4c75890022467d45194e82f4a4ada1a7
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-04-08 09:44:50 +0800

    使用使用gulp minify file

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp')
+var htmlmin = require("gulp-htmlmin")
+var cleancss = require('gulp-clean-css')
+var uglify = require('gulp-uglify')
+var pump = require('pump')
+var dir = './dist/public'
+gulp.task('minify-html', function (cb) {
+  var opts = {
+    collapseWhitespace: true,
+    preventLineBreaks: true,
+    removeComments: true,
+    removeEmptyAttributes: true,
+    minifyJS: true,
+    minifyCSS: true,
+    minifyURLs: true,
+  }
+  pump([
+    gulp.src('./web/**/*.html'),
+    htmlmin(opts),
+    gulp.dest(dir)
+  ], cb)
+})
+gulp.task('minify-css', function (cb) {
+  pump([
+    gulp.src('./web/**/*.css'),
+    cleancss({ compatibility: 'ie8' }),
+    gulp.dest(dir)
+  ])
+})
+gulp.task('minify-js', function (cb) {
+  pump([
+    gulp.src('./web/**/*.js'),
+    uglify(),
+    gulp.dest(dir)
+  ], cb)
+})
+gulp.task('default', ['minify-css', 'minify-js', 'minify-html'])
